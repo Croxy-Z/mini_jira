1ae2a6c1627b5f67e1c2f8affd534048409d24d7
croxyz <lexx.bar7@gmail.com>
2026-04-28 20:09:26 +0300

    feat(projects): add projects CRUD, configure pundit and handle unauthorized access

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,4 +16,45 @@ class ProjectsController < ApplicationController
     @project = current_user.projects.build
     authorize @project
   end
+
+  def edit
+    @project = Project.find(params[:id])
+    authorize @project
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    authorize @project
+
+    if @project.save
+      redirect_to @project, notice: "Project successfully created"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    authorize @project
+
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project successfully updated"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    authorize @project
+    @project.destroy
+
+    redirect_to projects_path, notice: "Project successfully deleted"
+  end
+
+  private
+
+  def project_params
+    params.expect(project: %i[title description])
+  end
 end
