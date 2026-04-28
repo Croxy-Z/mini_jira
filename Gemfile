bfbac28a333ec345b124feed15d56805c9c37f58
croxyz <lexx.bar7@gmail.com>
2026-04-28 17:04:46 +0300

    feat(auth): setup user roles and integrate pundit for authorization

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem "stimulus-rails"
 gem "devise"
 gem "haml-rails"
 gem "jbuilder"
+gem "pundit"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_27_212858) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_28_134054) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_27_212858) do
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.integer "role", default: 0
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
