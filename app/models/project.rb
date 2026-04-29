d4b344aec1c97b9953476236bef8b3d0a10c819c
croxyz <lexx.bar7@gmail.com>
2026-04-29 16:48:52 +0300

    feat(models): add DB not-null constraints and model validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,6 @@ class Project < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 1000 }, allow_blank: true
 end
