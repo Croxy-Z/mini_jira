8caf2e23d6812fac72ed1fe2acf9a81e07b817ae
croxyz <lexx.bar7@gmail.com>
2026-06-09 18:28:15 +0300

    test(user): add model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Roles
   enum :role, { user: 0, manager: 1, admin: 2 }
 
   has_many :projects, dependent: :destroy
