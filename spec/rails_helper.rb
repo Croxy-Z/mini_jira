3e53d4882839a58342f328bf0009b639c2c31074
croxyz <lexx.bar7@gmail.com>
2026-06-17 16:32:00 +0300

    test(system): configure Capybara in Docker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,13 +11,30 @@ require "shoulda/matchers"
 
 ActiveRecord::Migration.maintain_test_schema!
 
+Capybara.register_driver :selenium_chrome_headless_docker do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
+
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless_docker
+  end
+
   config.fixture_paths = [
     Rails.root.join("spec/fixtures")
   ]
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 
   config.use_transactional_fixtures = true
   config.filter_rails_from_backtrace!
