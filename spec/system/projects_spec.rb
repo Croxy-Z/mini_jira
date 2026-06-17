dcf4a9c848e04f57a70e62596a8cb8881f7e289d
croxyz <lexx.bar7@gmail.com>
2026-06-17 16:32:50 +0300

    test(tasks): cover turbo modal task creation

---
+++ spec/system/projects_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Projects" do
+  describe "task creation from project board" do
+    let(:user) { create(:user) }
+    let(:project) { create(:project, user:) }
+
+    before do
+      sign_in user
+    end
+
+    it "creates a task from the turbo modal" do
+      visit project_path(project)
+
+      click_link "Add Task"
+
+      expect(page).to have_css("turbo-frame#modal")
+      expect(page).to have_content("New task for #{project.title}")
+
+      fill_in "Title", with: "Write system spec"
+      fill_in "Description", with: "Cover Turbo modal task creation"
+
+      click_button "Create Task"
+
+      aggregate_failures do
+        expect(page).to have_content("Write system spec")
+        expect(page).to have_css("#tasks_to_do_count", text: "1")
+        expect(page).to have_no_content("New task for #{project.title}")
+      end
+    end
+  end
+end
