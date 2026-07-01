498e487f1fdb9ddabe9944e9f0a4e050620c4518
croxyz <lexx.bar7@gmail.com>
2026-07-01 18:55:32 +0300

    fix(auth): redirect to sign in after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,12 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
+  protected
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
   private
 
   def user_not_authorized
