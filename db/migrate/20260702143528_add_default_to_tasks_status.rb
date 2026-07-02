f26d4847f18b19120cda5e737bc78b3d2df8f771
croxyz <102975470+Croxy-Z@users.noreply.github.com>
2026-07-02 18:04:15 +0300

    Update migration to use ActiveRecord 8.1

--- db/migrate/20260702143528_add_default_to_tasks_status.rb
+++ db/migrate/20260702143528_add_default_to_tasks_status.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddDefaultToTasksStatus < ActiveRecord::Migration[8.0]
+class AddDefaultToTasksStatus < ActiveRecord::Migration[8.1]
   def change
     change_column_default :tasks, :status, from: nil, to: 0
   end
