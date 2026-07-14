47969039dedf6951fc1127fba541ddb464ad1dbd
croxyz <102975470+Croxy-Z@users.noreply.github.com>
2026-07-14 19:11:04 +0300

    fix(root): redirect guests to sign in without alert (#101)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "dashboard#index"
-
   devise_for :users
+
+  authenticated :user do
+    root "dashboard#index", as: :authenticated_root
+  end
+
+  root to: redirect("/users/sign_in", status: 302)
 
   resources :projects do
     resources :tasks, except: :index do
